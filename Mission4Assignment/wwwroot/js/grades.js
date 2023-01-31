c7110d2ac64117779aefc7a05abbb257fccb1d9f
bopellopa <bellak20@byu.edu>
2023-01-31 00:18:15 -0700

    changes

--- Mission4Assignment/wwwroot/js/grades.js
+++ Mission4Assignment/wwwroot/js/grades.js
@@ -1,6 +1,32 @@
 ﻿$("#grades").click(function () {
-    alert("assignments: " + $("#txtFrom").val() +
-        ", Subject: " + $("#txtSubject").val() +
-        ", Message: " + $("#txtMessage").val());
-    $("#picGanderson").fadeOut("slow");
+    totalscore = (parseInt($("#assn").val()) + parseInt($("#gp").val()) + parseInt($("#quiz").val()) + parseInt($("#mid").val()) + parseInt($("#final").val()) + parseInt($("#intex").val()))/6;
+    if (totalscore >= 94) {
+        grade = "A";
+    } else if(totalscore >= 90){
+        grade = "A-";
+    } else if(totalscore >= 87){
+        grade = "B+";
+    } else if(totalscore >= 84){
+        grade = "B";
+    } else if(totalscore >= 80){
+        grade = "B-";
+    } else if(totalscore >= 77){
+        grade = "C+";
+    } else if(totalscore >= 74){
+        grade = "C";
+    } else if(totalscore >= 70){
+        grade = "C-";
+    } else if(totalscore >= 67){
+        grade = "D+";
+    } else if(totalscore >= 64){
+        grade = "D";
+    } else if(totalscore >= 60){
+        grade = "D-";
+    } else {
+        grade = "E";
+    }
+    alert("overall grade: " + grade);
+    
+
+
 })
